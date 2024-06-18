ada1b5dce51887c3951a9d1951e0538af8ca4adf
Pirka Visual Studio <dphoto.hr@gmail.com>
2024-06-18 16:04:09 +0200

    loco

---
+++ src/scripts/loco.js
@@ -0,0 +1,40 @@
+import LocomotiveScroll from "locomotive-scroll";
+
+const locomotiveScroll = new LocomotiveScroll({
+  lenisOptions: {
+    wrapper: window,
+    content: document.documentElement,
+    lerp: 0.1,
+    duration: 1.2,
+    orientation: "vertical",
+    gestureOrientation: "vertical",
+    smoothWheel: true,
+    smoothTouch: false,
+    wheelMultiplier: 2,
+    touchMultiplier: 2,
+    normalizeWheel: true,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  },
+});
+
+/* Nav color */
+window.addEventListener("scrollNavColor", (e) => {
+  const { target, way } = e.detail;
+
+  if (way === "enter") {
+    target.style.backgroundColor = "";
+  } else {
+    target.style.backgroundColor = "rgba(255,255,255,0.7)";
+  }
+});
+
+/* nav height */
+window.addEventListener("scrollNavHeight", (e) => {
+  const { target, way } = e.detail;
+
+  if (way === "enter") {
+    target.style.height = "";
+  } else {
+    target.style.height = "4rem";
+  }
+});
