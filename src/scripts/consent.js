b96f88884e3ac3195578778298fa0e3bc4cb2010
Pirka Visual Studio <dphoto.hr@gmail.com>
2024-07-01 12:15:01 +0200

    consent

--- src/scripts/consent.js
+++ src/scripts/consent.js
@@ -1,7 +1,10 @@
+// HIDE THE BANNER FUNCTION
+
 function hideBanner() {
   document.getElementById("cookie-consent-banner").style.display = "none";
 }
 
+// FUNCTION FOR CONSENT STATES
 if (localStorage.getItem("consentMode") === null) {
   document
     .getElementById("btn-accept-all")
@@ -9,10 +12,22 @@ if (localStorage.getItem("consentMode") === null) {
       setConsent({
         necessary: true,
         analytics: true,
-        preferences: true,
+        personalization: true,
         marketing: true,
       });
+
+      //HIDE BANNER AFTER CLICK
       hideBanner();
+      // PUSH THE DATA TO DATA LAYER FOR GOOGLE TAG
+      dataLayer.push({
+        funcionality_storage: "true",
+        ad_storage: "true",
+        ad_user_data: "true",
+        ad_personalization: "true",
+        analytics_storage: "true",
+
+        event: "consentEvent",
+      });
     });
 
   document
@@ -21,22 +36,46 @@ if (localStorage.getItem("consentMode") === null) {
       setConsent({
         necessary: true,
         analytics: document.getElementById("consent-analytics").checked,
-        preferences: document.getElementById("consent-preferences").checked,
-        marketing: document.getElementById("marketing").checked,
+        personalization: document.getElementById("consent-personalization")
+          .checked,
+        marketing: document.getElementById("consent-marketing").checked,
       });
+      //HIDE BANNER AFTER CLICK
       hideBanner();
+      // PUSH THE DATA TO DATA LAYER FOR GOOGLE TAG
+      dataLayer.push({
+        funcionality_storage: "true",
+        ad_storage: document.getElementById("consent-marketing").checked,
+        ad_user_data: document.getElementById("consent-marketing").checked,
+        ad_personalization: document.getElementById("consent-personalization")
+          .checked,
+        analytics_storage: document.getElementById("consent-analytics").checked,
+
+        event: "consentEvent",
+      });
     });
 
   document
     .getElementById("btn-reject-all")
     .addEventListener("click", function () {
       setConsent({
-        necessary: false,
+        necessary: true,
         analytics: false,
-        preferences: false,
+        personalization: false,
         marketing: false,
       });
+      //HIDE BANNER AFTER CLICK
       hideBanner();
+      // PUSH THE DATA TO DATA LAYER FOR GOOGLE TAG
+      dataLayer.push({
+        funcionality_storage: "true",
+        ad_storage: "false",
+        ad_user_data: "false",
+        ad_personalization: "false",
+        analytics_storage: "false",
+
+        event: "consentEvent",
+      });
     });
 
   document.getElementById("cookie-consent-banner").style.display = "block";
@@ -45,11 +84,55 @@ if (localStorage.getItem("consentMode") === null) {
 function setConsent(consent) {
   const consentMode = {
     funcionality_storage: consent.necessary ? "granted" : "denied",
-    security_storage: consent.necessary ? "granted" : "denied",
-    ad_storage: consent.necessary ? "granted" : "denied",
-    analytics_storage: consent.necessary ? "granted" : "denied",
-    personalization: consent.necessary ? "granted" : "denied",
+    ad_storage: consent.marketing ? "granted" : "denied",
+    ad_user_data: consent.marketing ? "granted" : "denied",
+    ad_personalization: consent.personalization ? "granted" : "denied",
+    analytics_storage: consent.analytics ? "granted" : "denied",
   };
   gtag("consent", "update", consent);
+
   localStorage.setItem("consentMode", JSON.stringify(consentMode));
+}
+
+const consentData = JSON.parse(localStorage.getItem("consentMode"));
+
+// LOAD THE DATA LAYER STATES ON INITIALIZATION
+if (consentData.funcionality_storage === "granted") {
+  dataLayer.push({
+    funcionality_storage: "true",
+
+    event: "consentEvent",
+  });
+}
+
+if (consentData.ad_storage === "granted") {
+  dataLayer.push({
+    ad_storage: "true",
+
+    event: "consentEvent",
+  });
+}
+
+if (consentData.ad_user_data === "granted") {
+  dataLayer.push({
+    ad_user_data: "true",
+
+    event: "consentEvent",
+  });
+}
+
+if (consentData.ad_personalization === "granted") {
+  dataLayer.push({
+    ad_personalization: "true",
+
+    event: "consentEvent",
+  });
+}
+
+if (consentData.analytics_storage === "granted") {
+  dataLayer.push({
+    analytics_storage: "true",
+
+    event: "consentEvent",
+  });
 }
