a4d46ad0bb3766e56b167ee8d3a9a86275921e02
Pirka Visual Studio <dphoto.hr@gmail.com>
2025-02-06 14:51:19 +0100

    Reviews

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -4,20 +4,10 @@ var splide = new Splide(".splide", {
   type: "loop",
   drag: "free",
   snap: true,
-  perPage: 4,
+  perPage: 1,
   gap: "0.5rem",
   focus: "center",
   updateOnMove: true,
-  breakpoints: {
-    1024: {
-      perPage: 3,
-      gap: "0.5rem",
-    },
-    768: {
-      perPage: 1,
-      gap: "0.5rem",
-    },
-  },
 });
 
 splide.mount();
