8cde6d7aa590d98f1e1d4fdacb2e66aa247caaa9
Pirka Visual Studio <dphoto.hr@gmail.com>
2025-12-03 10:54:53 +0100

    theme change

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -2,15 +2,15 @@ const button = document.getElementById("theme-toggle");
 const html = document.documentElement;
 
 // Load stored theme or default to dark
-const currentTheme = localStorage.getItem("theme") || "theme-dark";
+const currentTheme = localStorage.getItem("theme") || "theme-light";
 html.classList.add(currentTheme);
 
 button.addEventListener("click", () => {
-  if (html.classList.contains("theme-dark")) {
-    html.classList.replace("theme-dark", "theme-light");
-    localStorage.setItem("theme", "theme-light");
-  } else {
+  if (html.classList.contains("theme-light")) {
     html.classList.replace("theme-light", "theme-dark");
     localStorage.setItem("theme", "theme-dark");
+  } else {
+    html.classList.replace("theme-dark", "theme-light");
+    localStorage.setItem("theme", "theme-light");
   }
 });
