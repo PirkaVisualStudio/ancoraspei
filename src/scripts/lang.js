00fe7e65003a39be66ae5b72cab33bdfbd96f6c9
Pirka Visual Studio <dphoto.hr@gmail.com>
2026-08-12 23:18:33 +0200

    sve

--- src/scripts/lang.js
+++ src/scripts/lang.js
@@ -3,11 +3,11 @@ document.getElementById("to-hr").addEventListener("click", (e) => {
   const maxAge = 60 * 60 * 24 * 365;
   document.cookie = `lang=hr; path=/; max-age=${maxAge}; SameSite=Lax;`;
   // Optionally redirect user to /hr
-  window.location.href = "/hr";
+  window.location.href = "/";
 });
 
 document.getElementById("to-en").addEventListener("click", () => {
   const maxAge = 60 * 60 * 24 * 365;
   document.cookie = `lang=en; path=/; max-age=${maxAge}; samesite=lax`;
-  window.location.href = "/";
+  window.location.href = "/en";
 });
